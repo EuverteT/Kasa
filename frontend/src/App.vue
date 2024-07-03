03ff1305c2e3437bcf5210729c01b8fd4adcd77c
EuverteT <thomaseuverte@gmail.com>
2024-07-03 19:33:03 +0200

    Ajout Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import { RouterView } from 'vue-router'
 
 
@@ -9,7 +10,7 @@ import { RouterView } from 'vue-router'
 <template>
    <Header />
    <RouterView />
-   
+   <Footer />
 </template>
 
 <style scoped>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="footer">
+    <img alt="Logo Kasa Black" class="logo" src="@/assets/kasa-black.png" width="125" height="50" />
+  
+    <div>2024 Kasa. All rights reserved.</div>
+  </div>
+  
+</template>
+
+<style scoped lang=scss>
+
+@import "./src/assets/main.scss";
+
+.footer {
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 1rem;
+  background: black;
+  color: white;
+
+  }
+
+</style>
