c8ea4637de09ad2d8c77e1c9c2473701fd81f911
EuverteT <thomaseuverte@gmail.com>
2024-07-08 12:27:58 +0200

    Changement du positionnement Horloge

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -7,11 +7,11 @@ import HeureCourante from '../components/HeureCourante.vue';
 <template>
   <div class="header">
     <img alt="Logo Kasa" class="logo" src="@/assets/kasa.png" width="125" height="50" />
-    <HeureCourante />
 
   <nav>
     <RouterLink to="/">Accueil</RouterLink>
     <RouterLink to="/about">A propos</RouterLink>
+    <HeureCourante />
   </nav>
 
   </div>
@@ -28,18 +28,28 @@ import HeureCourante from '../components/HeureCourante.vue';
   justify-content: space-between;
   margin: 1rem;
 
-  nav a {
-    font-size: 1.5rem;
-    font-weight: 800;
-    padding: 0.8rem;
-    margin: 0 1rem;
-    background: $mid-grey;
-    border-radius: 0.5rem;
+  nav  {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
-    &:hover {
+    a {
+      font-size: 1.5rem;
+      font-weight: 800;
+      padding: 0.8rem;
+      margin: 0 1rem;
+      background: $mid-grey;
+      border-radius: 0.5rem;
+
+      &:hover {
       background: $hard-grey;
 
     }
+
+    }
+    
+
+    
 
   }
 }
