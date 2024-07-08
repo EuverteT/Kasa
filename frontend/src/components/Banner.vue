835a996276a03afb9c5abd3274112908a52bccd4
EuverteT <thomaseuverte@gmail.com>
2024-07-08 14:19:12 +0200

    Ajout du texte sur Banner

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -1,45 +1,47 @@
 <script>
 
-
 export default{
     name: "Banner",
-
     data(){
-      return {
-      
+      return {     
       aboutSrc: "src/assets/aboutBanner.png",
-      homeSrc: "src/assets/homeBanner.png"
-      
-      }
-      
+      homeSrc: "src/assets/homeBanner.png"     
+      } 
     },
     props: {
       image: String
-    }
-    
+    }  
 }
 </script>
 
 <template>
   <div class="banner">
     <img :src=image >
+    <span>Chez vous, partout et ailleurs</span>
   </div>
-  
 </template>
 
 <style scoped lang=scss>
 
 @import "./src/assets/main.scss";
-
 .banner {
-    display: flex;
-    justify-content: center;
-    margin: 1rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+
+  span {
+    color: white;
+    position: absolute;
+    font-size: 3rem;
+  }
 }
 
 .banner img {
-    width: 100%;
-    object-fit: cover;
+  width: 100%;
+  object-fit: cover;
+  filter: brightness(0.6);
+  border-radius: 1rem
 }
-
 </style>
